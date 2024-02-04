5a7a3c87887e1498a12d0817ac9715d659a24e08
Abhi <122604718+Abhinov037@users.noreply.github.com>
2024-02-04 18:39:16 +0530

    main

---
+++ introduction.cc/coding-ninjas-questions.cc/functions.cc/Total-Prime.cc
@@ -0,0 +1,28 @@
+/* Problem statement
+
+Write a program to find the total number of a primes number in a given interval.
+
+Given two integers S and E, count all primes between S and E.
+*/
+
+
+
+#include <bits/stdc++.h> 
+#include <iostream>
+
+using namespace std;
+
+int totalPrime(int S,int E){
+    for(int i=S;i<E)
+    
+
+
+
+}
+
+int main() {
+    int S, E;
+    cin >> S >> E;
+    cout << totalPrime(S, E);
+    return 0;
+}
