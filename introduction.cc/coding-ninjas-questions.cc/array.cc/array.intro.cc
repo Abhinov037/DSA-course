79f5a505e2f11b32520604f047b75fd8aea8e177
Abhi <122604718+Abhinov037@users.noreply.github.com>
2024-02-19 19:30:20 +0530

    main

---
+++ introduction.cc/coding-ninjas-questions.cc/array.cc/array.intro.cc
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //declare
+    int number[15];
+    
+
+    //accessing an array 
+    cout<<"value at 14 index"<<number[14]<<endl;
+
+
+    //initialising an array 
+    int second[3]={5,7,11};
+
+    //accessing an element
+    cout<<"value at 2 index"<<second[2]<<endl;
+
+    int third[15]={2,7};
+
+    int n=15;
+    cout<<"printing the array"<<endl;
+
+    //print the array 
+    for(int i=0;i<n;i++){
+   
+   cout<<third[i]<<" ";
+    }
+
+
+//initilising all locations with 0
+    int fourth[10]={0};
+    n=15;
+    cout<<"printing the array"<< endl;
+  //print the array 
+     for(int i=0;i<n;i++){
+   
+   cout<<fourth[i]<<" ";
+    }
+
+//initilising all locations with 1 [ not possible]
+    int fifth[10]={1};
+    n=15;
+    cout<<"printing the array"<< endl;
+  //print the array 
+     for(int i=0;i<n;i++){
+   
+   cout<<fifth[i]<<" ";
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
